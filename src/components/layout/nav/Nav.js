c344f614d21480d3138059735eb67c6996c287b7
mevanmaele <mevanmaele@gmail.com>
2020-01-17 15:10:51 -0500

    Make nav a little nicer

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -18,14 +18,16 @@ const Nav = props => {
     >
       <div className={classNames(styles.content, "pageContainer", "noPadding")}>
         <img src={logo} alt="GIDA - Tracker" />
-        <Link to="/explore/map">Map</Link>
-        <Link to="/explore/org">Organizations</Link>
-        <Link to="/analysis">Analysis</Link>
-        <Link to="/data">Data</Link>
-        <Link to="/about/background">Background</Link>
-        <Link to="/about/data">Data sources</Link>
-        <Link to="/about/research">Research</Link>
-        <Link to="/about/submit">Submit data</Link>
+        <div className={styles.links}>
+          <Link to="/explore/map">Map</Link>
+          <Link to="/explore/org">Organizations</Link>
+          <Link to="/analysis">Analysis</Link>
+          <Link to="/data">Data</Link>
+          <Link to="/about/background">Background</Link>
+          <Link to="/about/data">Data sources</Link>
+          <Link to="/about/research">Research</Link>
+          <Link to="/about/submit">Submit data</Link>
+        </div>
       </div>
     </div>
   );
