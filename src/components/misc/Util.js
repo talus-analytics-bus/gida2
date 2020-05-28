8f1e145290e10d3651c1f8bc6f0b6708b2c99915
mevanmaele <mvanmaele@talusanalytics.com>
2020-05-28 10:53:31 -0400

    update last updated date

--- src/components/misc/Util.js
+++ src/components/misc/Util.js
@@ -741,10 +741,10 @@ Util.today = () => {
 Util.lastUpdated = ({ type = "all" }) => {
   switch (type) {
     case "iati":
-      return new Date("2020-04-12 12:00 PM");
+      return new Date("2020-05-27 12:00 PM");
     case "all":
     default:
-      return new Date("2020-04-12 12:00 PM");
+      return new Date("2020-05-27 12:00 PM");
   }
 };
 
