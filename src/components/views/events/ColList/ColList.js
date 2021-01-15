0e10d38e1faaf1d88ca36dc964cf9b65b3b3ee7d
mvanmaele <mvanmaele@talusanalytics.com>
2021-01-14 20:24:54 -0500

    Mark certain components as placeholders

--- src/components/views/events/ColList/ColList.js
+++ src/components/views/events/ColList/ColList.js
@@ -1,6 +1,6 @@
 // 3rd party libs
 import React, { useState, useEffect } from "react";
 const ColList = ({}) => {
-  return <div>ColList</div>;
+  return <div>[PLACEHOLDER] ColList</div>;
 };
 export default ColList;
--- src/components/views/events/PathogenBox/PathogenBox.js
+++ src/components/views/events/PathogenBox/PathogenBox.js
@@ -1,6 +1,6 @@
 // 3rd party libs
 import React, { useState, useEffect } from "react";
 const PathogenBox = ({}) => {
-  return <div>PathogenBox</div>;
+  return <div>[PLACEHOLDER] PathogenBox</div>;
 };
 export default PathogenBox;
