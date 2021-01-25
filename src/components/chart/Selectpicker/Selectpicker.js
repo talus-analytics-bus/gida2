762ab84ddb678d8cc13ff934e23cdf24c8c171e5
mvanmaele <mvanmaele@talusanalytics.com>
2021-01-25 13:32:11 -0500

    Add support for `optgroup` tags in `Selectpicker` component

--- src/components/chart/Selectpicker/Selectpicker.js
+++ src/components/chart/Selectpicker/Selectpicker.js
@@ -11,6 +11,7 @@ import Util from "../../misc/Util.js";
 const Selectpicker = ({
   setOption = () => "",
   optionList = [],
+  optionGroups,
   allOption,
   label = "Label",
   curSelection = "test",
@@ -19,14 +20,34 @@ const Selectpicker = ({
   const handleChange = e => {
     setOption(e.target.value);
   };
+  const isGrouped = optionGroups !== undefined;
+
+  // FUNCTIONS //
+  // return JSX for options, including optgroups if defined
+  const getOptionJsx = () => {
+    if (isGrouped) {
+      const optionJsx = [];
+      for (const [k, v] of Object.entries(optionGroups)) {
+        optionJsx.push(
+          <optgroup label={k}>
+            {v.map(d => (
+              <option value={d.value}>{d.label}</option>
+            ))}
+          </optgroup>
+        );
+      }
+      return optionJsx;
+    } else {
+      return optionList.map(d => <option value={d.value}>{d.label}</option>);
+    }
+  };
+  const optionJsx = getOptionJsx();
   return (
     <div className={styles.selectpicker}>
       <div className={styles.label}>{label}</div>
       <select value={curSelection} onChange={handleChange}>
         {allOption && <option value="all">{allOption}</option>}
-        {optionList.map(d => (
-          <option value={d.value}>{d.label}</option>
-        ))}
+        {optionJsx}
       </select>
     </div>
   );
