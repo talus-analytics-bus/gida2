c20586de052a5e60539f80d9afd80f4efa4a1b9a
mvanmaele <mvanmaele@talusanalytics.com>
2021-01-26 09:56:00 -0500

    Set up placeholder components for crossreferences section at bottom of event page

---
+++ src/components/views/events/Crossreferences/Carousel/Carousel.js
@@ -0,0 +1,13 @@
+// 3rd party libs
+import React, { useState, useEffect } from "react";
+
+// styles and assets
+import styles from "./carousel.module.scss";
+
+// local components
+import { GOAL_URL, GOAL_API_URL } from "../Crossreferences";
+
+const Carousel = ({}) => {
+  return <div>Carousel</div>;
+};
+export default Carousel;
--- src/components/views/events/Crossreferences/Crossreferences.js
+++ src/components/views/events/Crossreferences/Crossreferences.js
@@ -1,5 +1,10 @@
 // 3rd party libs
 import React, { useState, useEffect } from "react";
+
+// constants
+export const GOAL_URL = "https://goal.ghscosting.org/";
+export const GOAL_API_URL = "https://goal-api.talusanalytics.com/";
+
 const Crossreferences = ({}) => {
   return <div>Crossreferences</div>;
 };
