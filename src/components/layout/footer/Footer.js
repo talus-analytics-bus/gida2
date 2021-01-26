a30696727725e6bbc553a400069699b06ad3dd4e
Nate Bennett <nbennett@talusanalytics.com>
2021-01-26 13:23:15 -0700

    Favicon updated and footer done for now (still have to switch out dark mode version of logos)

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -5,55 +5,51 @@ import talus from "../../../assets/images/logo-talus.png";
 import talusDark from "../../../assets/images/logo-talus-dark.png";
 import gu from "../../../assets/images/logo-georgetown.png";
 import guDark from "../../../assets/images/logo-georgetown-dark.png";
+import idea from "../../../assets/images/logo-title.png";
 import Util from "../../misc/Util.js";
 
 const Footer = ({ ...props }) => {
   const images = [
     {
+      imgSrc: props.isDark ? idea : idea, // TODO: add idea dark mode version
+      url: "https://ghssidea.org",
+      alt: "International Disease and Events Analysis",
+    },
+    {
       imgSrc: props.isDark ? guDark : gu,
       url: "https://ghss.georgetown.edu/",
       alt:
         "Georgetown University Center for Global Health Science and Security",
-      txt: null
     },
     {
       imgSrc: props.isDark ? talusDark : talus,
       url: "http://talusanalytics.com/",
       alt: "Talus Analytics, LLC",
-      txt: "Built by",
-      style: {
-        height: "60px",
-        position: "relative",
-        top: "5px"
-      }
-    }
+    },
   ];
 
   return (
     <div
       className={classNames(styles.footer, {
         [styles.dark]: props.isDark,
-        [styles.wide]: props.isWide
+        [styles.wide]: props.isWide,
       })}
     >
       <div className={styles.content}>
-        {
-          <div className={styles.dataAsOf}>
-            Data last updated{" "}
-            {Util.lastUpdated({}).toLocaleString("en-US", {
-              month: "short",
-              year: "numeric",
-              day: "numeric"
-            })}
-          </div>
-        }
         <div className={styles.links}>
-          {images.map(d => (
-            <a target="_blank" href={d.url} alt={d.alt}>
-              <img style={d.style} src={d.imgSrc} />
-              {d.txt && <div>{d.txt}</div>}
+          {images.map((d) => (
+            <a target="_blank" href={d.url}>
+              <img src={d.imgSrc} alt={d.alt} />
             </a>
           ))}
+        </div>
+        <div className={styles.dataAsOf}>
+          Data last updated{" "}
+          {Util.lastUpdated({}).toLocaleString("en-US", {
+            month: "short",
+            year: "numeric",
+            day: "numeric",
+          })}
         </div>
       </div>
     </div>
