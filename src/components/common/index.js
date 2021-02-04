c83d150070f8e695e29e04be0d41e282be7eaab7
mvanmaele <mvanmaele@talusanalytics.com>
2021-02-04 10:56:06 -0500

    Add index file for all common components

---
+++ src/components/common/index.js
@@ -0,0 +1,28 @@
+import Button from "./Button/Button.js";
+import FilterSelections from "./FilterSelections/FilterSelections.js";
+import Search from "./Search/Search.js";
+import Checkbox from "./Checkbox/Checkbox.js";
+import GhsaButton from "./GhsaButton/GhsaButton.js";
+import SlideToggle from "./SlideToggle/SlideToggle.js";
+import Chevron from "./Chevron/Chevron.js";
+import Loading from "./Loading/Loading.js";
+import SourceText from "./SourceText/SourceText.js";
+import Drawer from "./Drawer/Drawer.js";
+import Pagination from "./Pagination/Pagination.js";
+import FilterDropdown from "./FilterDropdown/FilterDropdown.js";
+import ScoreBlocks from "./ScoreBlocks/ScoreBlocks.js";
+export {
+  Button,
+  FilterSelections,
+  Search,
+  Checkbox,
+  GhsaButton,
+  SlideToggle,
+  Chevron,
+  Loading,
+  SourceText,
+  Drawer,
+  Pagination,
+  FilterDropdown,
+  ScoreBlocks,
+};
