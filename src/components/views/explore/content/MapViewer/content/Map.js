d915f4551ae0e2d58ae7b8919813005621bbbfcc
mvanmaele <mvanmaele@talusanalytics.com>
2021-02-09 13:25:22 -0500

    Define key for InfoBox component

--- src/components/views/explore/content/MapViewer/content/Map.js
+++ src/components/views/explore/content/MapViewer/content/Map.js
@@ -398,6 +398,7 @@ const Map = ({
             tooltipData && (
               <InfoBox
                 {...{
+                  key: tooltipNodeData.id,
                   simple: true,
                   entityRole,
                   supportType,
