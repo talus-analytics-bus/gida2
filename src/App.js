7b80ca838bdc0c6b78aca5f1b8a786b1575cbf37
mvanmaele <mvanmaele@talusanalytics.com>
2021-02-11 21:35:21 -0500

    Fix bug where Org page nav bar item didn't become bold when appropriate

--- src/App.js
+++ src/App.js
@@ -301,7 +301,7 @@ const App = () => {
                 exact
                 path="/explore/org"
                 render={d => {
-                  setPage("explore-" + d.match.params.activeTab);
+                  setPage("explore-org");
                   setExploreComponent(null);
                   // Get support type if specified.
                   const urlParams = new URLSearchParams(d.location.search);
