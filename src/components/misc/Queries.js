c76b251feedc1389b5c767af9f8d436f864e0fcb
mvanmaele <mvanmaele@talusanalytics.com>
2021-05-26 10:39:25 -0400

    Remove debugger call

--- src/components/misc/Queries.js
+++ src/components/misc/Queries.js
@@ -380,7 +380,6 @@ export const Excel = async function({
     if (data === null) {
       console.error("Error: `data` is required for method POST.")
     }
-    debugger
     req = axios({
       url: `${API_URL}/post/${route}`,
       method: "POST",
