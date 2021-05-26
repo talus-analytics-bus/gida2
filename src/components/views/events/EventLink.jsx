bfe7bdc9137507bfc2e9ee177f73bad8bfdb4af7
mvanmaele <mvanmaele@talusanalytics.com>
2021-05-26 10:28:18 -0400

    Unlink PHEIC names in tables

--- src/components/views/events/EventLink.jsx
+++ src/components/views/events/EventLink.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export function EventLink({ name, slug }) {
-    return <Link {...{
-        to: `/events/${slug}`
-    }}>{name}</Link>;
+export function EventLink({ name, slug, noLink = true }) {
+    if (!noLink)
+        return <Link {...{
+            to: `/events/${slug}`
+        }}>{name}</Link>;
+    else return name;
 }
