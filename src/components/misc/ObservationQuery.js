1bd090a31bfc99f0c27578ba5f3eb92eb6e04fed
mvanmaele <mvanmaele@talusanalytics.com>
2021-07-07 16:09:19 -0400

    Fix bug where wrong field checked for version name

--- src/components/misc/ObservationQuery.js
+++ src/components/misc/ObservationQuery.js
@@ -1,14 +1,14 @@
 // 3rd party libs
-import axios from "axios";
+import axios from "axios"
 
 // utility libs
-import Util from "./Util.js";
+import Util from "./Util.js"
 
 // constants
-const API_URL = process.env.REACT_APP_METRICS_API_URL;
-const AMP_METRIC_API_URL = process.env.REACT_APP_AMP_METRICS_API_URL;
-const AMP_API_URL = "https://api.covidamp.org";
-const INFINITY = 1e9;
+const API_URL = process.env.REACT_APP_METRICS_API_URL
+const AMP_METRIC_API_URL = process.env.REACT_APP_AMP_METRICS_API_URL
+const AMP_API_URL = "https://api.covidamp.org"
+const INFINITY = 1e9
 
 /**
  * Get observation data from API. Updates the observation data and loading status
@@ -29,58 +29,58 @@ const ObservationQuery = async function({
 }) {
   // FUNCTIONS //
   const getLastDatumDate = async isAMP => {
-    if (!isAMP) return null;
+    if (!isAMP) return null
     else {
       const res = await axios({
         url: `${AMP_API_URL}/get/version`,
-      });
+      })
       return res.data.data.find(
-        d => d.type === "COVID-19 case data (countries)"
-      ).last_datum_date;
+        d => d.name === "COVID-19 case data (countries)",
+      ).last_datum_date
     }
-  };
+  }
 
   // CONSTANTS //
-  end_date = typeof end_date !== "undefined" ? end_date : start_date;
+  end_date = typeof end_date !== "undefined" ? end_date : start_date
   const last_datum_date = await getLastDatumDate(
-    metric_id === 75 || metric_id === 95
-  );
+    metric_id === 75 || metric_id === 95,
+  )
   if (last_datum_date !== null) {
-    end_date = last_datum_date;
-    start_date = last_datum_date;
+    end_date = last_datum_date
+    start_date = last_datum_date
   }
   var params = {
     metric_id,
     temporal_resolution,
     spatial_resolution,
     // lag_allowed: INFINITY,
-  };
+  }
 
   // Send start and end dates if they are provided, otherwise do not send.
-  if (end_date !== undefined) params.start = end_date;
-  if (start_date !== undefined) params.end = start_date;
-  if (fields !== undefined) params.fields = fields.join(",");
+  if (end_date !== undefined) params.start = end_date
+  if (start_date !== undefined) params.end = start_date
+  if (fields !== undefined) params.fields = fields.join(",")
 
   if (place_id !== undefined) {
-    params["place_id"] = place_id;
+    params["place_id"] = place_id
   } else if (place_name !== undefined) {
-    params["place_name"] = place_name;
+    params["place_name"] = place_name
   } else if (place_iso3 !== undefined) {
-    params["place_iso3"] = place_iso3;
+    params["place_iso3"] = place_iso3
   }
 
   const url =
     metric_id === 75 || metric_id === 95
       ? `${AMP_METRIC_API_URL}/observations`
-      : `${API_URL}/observations`;
+      : `${API_URL}/observations`
 
   const res = await axios({
     url,
     params,
     headers: { "If-Modified-Since": new Date().toUTCString() },
-  });
+  })
 
-  return res.data.data;
-};
+  return res.data.data
+}
 
-export default ObservationQuery;
+export default ObservationQuery
