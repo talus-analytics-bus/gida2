b0a2cfa5021de570be99d14cec9c0f61074db42d
mvanmaele <mvanmaele@talusanalytics.com>
2022-03-08 17:31:17 -0500

    Add local version of build

---
+++ build-local/precache-manifest.2deae56e3af6d5ce15b2bd0152093164.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5da14761dbebff996377020ab86143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5cd9c04ac531b494b8d",
+    "url": "/static/css/2.0a421cff.chunk.css"
+  },
+  {
+    "revision": "67e88834718530a8a1a3",
+    "url": "/static/css/main.4b7a8178.chunk.css"
+  },
+  {
+    "revision": "d5cd9c04ac531b494b8d",
+    "url": "/static/js/2.f40456e0.chunk.js"
+  },
+  {
+    "revision": "67e88834718530a8a1a3",
+    "url": "/static/js/main.b4169e5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0a97c4ff2a3a9c40a98b78991c47ede",
+    "url": "/static/media/GOTHAMBOOK.a0a97c4f.woff"
+  },
+  {
+    "revision": "e7d329add00ca9f8efb769cd449b1ac8",
+    "url": "/static/media/Gotham Bold.e7d329ad.woff"
+  },
+  {
+    "revision": "0062c34665a3fc0f2278cd4e955702ec",
+    "url": "/static/media/OpenSans-Bold.0062c346.ttf"
+  },
+  {
+    "revision": "3c5d7848566d8c9226a085610f951bdc",
+    "url": "/static/media/OpenSans-Bold.3c5d7848.woff"
+  },
+  {
+    "revision": "4e2cb392634cd44eddfc7d6fd40ff466",
+    "url": "/static/media/OpenSans-Bold.4e2cb392.woff2"
+  },
+  {
+    "revision": "d38f69da9495add6b25083a9f5c9b6b6",
+    "url": "/static/media/OpenSans-Bold.d38f69da.eot"
+  },
+  {
+    "revision": "91d189b69a629f7a7da8a5758daae44e",
+    "url": "/static/media/OpenSans-BoldItalic.91d189b6.woff"
+  },
+  {
+    "revision": "9ce03dbcba3289a39bac4145d4621368",
+    "url": "/static/media/OpenSans-BoldItalic.9ce03dbc.woff2"
+  },
+  {
+    "revision": "ac92bed205cd66a2a648ffc0f65f2a29",
+    "url": "/static/media/OpenSans-BoldItalic.ac92bed2.ttf"
+  },
+  {
+    "revision": "bd5470c94943c3e954f94aed5d6b7d59",
+    "url": "/static/media/OpenSans-BoldItalic.bd5470c9.eot"
+  },
+  {
+    "revision": "20ab65ceaffd1c888977f386f96e2540",
+    "url": "/static/media/OpenSans-Italic.20ab65ce.ttf"
+  },
+  {
+    "revision": "6ced78784c60814a4db3ce5b015c649a",
+    "url": "/static/media/OpenSans-Italic.6ced7878.woff"
+  },
+  {
+    "revision": "e68fe0bb617ae83d3e98e3a10f363418",
+    "url": "/static/media/OpenSans-Italic.e68fe0bb.woff2"
+  },
+  {
+    "revision": "f5576ae56659f65bc0e77e7ed86c8d1a",
+    "url": "/static/media/OpenSans-Italic.f5576ae5.eot"
+  },
+  {
+    "revision": "3bcacc0bfa02a1b4bdeb190a2c9df3dc",
+    "url": "/static/media/OpenSans-Regular.3bcacc0b.eot"
+  },
+  {
+    "revision": "4f9c870c28f9db0c1f7e9b7a7e72f3a9",
+    "url": "/static/media/OpenSans-Regular.4f9c870c.woff2"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "/static/media/OpenSans-Regular.5a798cda.ttf"
+  },
+  {
+    "revision": "bc3dcd61db2d016cd60546927c1b5abd",
+    "url": "/static/media/OpenSans-Regular.bc3dcd61.woff"
+  },
+  {
+    "revision": "7434d4445c73444df4c3c27c21e29345",
+    "url": "/static/media/OpenSans-Semibold.7434d444.eot"
+  },
+  {
+    "revision": "7b7c699ca0a4fd152ef4c48d6828aa1a",
+    "url": "/static/media/OpenSans-Semibold.7b7c699c.ttf"
+  },
+  {
+    "revision": "b00dff9ec6f356c2f8eb47a7c566c1f2",
+    "url": "/static/media/OpenSans-Semibold.b00dff9e.svg"
+  },
+  {
+    "revision": "dc95b864b51e1551c77c1f1adb4c3601",
+    "url": "/static/media/OpenSans-Semibold.dc95b864.woff"
+  },
+  {
+    "revision": "250be06d697903680e6ee9f3bb5ce69b",
+    "url": "/static/media/Raleway-Bold.250be06d.woff"
+  },
+  {
+    "revision": "2f99a85426a45e0c7f8707aae53af803",
+    "url": "/static/media/Raleway-Bold.2f99a854.ttf"
+  },
+  {
+    "revision": "3a80587b4415f8b0b21cdeb8e380a86c",
+    "url": "/static/media/Raleway-Bold.3a80587b.eot"
+  },
+  {
+    "revision": "3e9b22e57fd6467de3e5e99420f75b45",
+    "url": "/static/media/Raleway-Bold.3e9b22e5.woff2"
+  },
+  {
+    "revision": "02b5ea3c708f1295689f09bc0b555e7b",
+    "url": "/static/media/Raleway-BoldItalic.02b5ea3c.eot"
+  },
+  {
+    "revision": "408bba452435caefaae12c6351c56946",
+    "url": "/static/media/Raleway-BoldItalic.408bba45.woff"
+  },
+  {
+    "revision": "51e9eba593a92336943377334b688904",
+    "url": "/static/media/Raleway-BoldItalic.51e9eba5.ttf"
+  },
+  {
+    "revision": "bf23a8a4a731b633cce8315c02c4a279",
+    "url": "/static/media/Raleway-BoldItalic.bf23a8a4.woff2"
+  },
+  {
+    "revision": "5a50539dd07c640c7694c5d24824b559",
+    "url": "/static/media/Raleway-Italic.5a50539d.eot"
+  },
+  {
+    "revision": "81ba0f0bf459cd621832e5c272e72a4f",
+    "url": "/static/media/Raleway-Italic.81ba0f0b.woff"
+  },
+  {
+    "revision": "9634328a9ea7185e1160e87cfaf88e03",
+    "url": "/static/media/Raleway-Italic.9634328a.woff2"
+  },
+  {
+    "revision": "e97be495a4a423cf1dd376b9becca841",
+    "url": "/static/media/Raleway-Italic.e97be495.ttf"
+  },
+  {
+    "revision": "30a444afc423dde0beab8d856d302199",
+    "url": "/static/media/Raleway-Regular.30a444af.woff"
+  },
+  {
+    "revision": "84abe14c9756256a4b91300ba3e4ec62",
+    "url": "/static/media/Raleway-Regular.84abe14c.ttf"
+  },
+  {
+    "revision": "ba53f4635e86701840e33e9de74ed1c5",
+    "url": "/static/media/Raleway-Regular.ba53f463.woff2"
+  },
+  {
+    "revision": "be5e6c1cc25bfbd6d92647d56aae28d4",
+    "url": "/static/media/Raleway-Regular.be5e6c1c.eot"
+  },
+  {
+    "revision": "3a799f6b2898d7e7cd0e843038fe99fa",
+    "url": "/static/media/arrow.3a799f6b.svg"
+  },
+  {
+    "revision": "2ac492692944ec521cc3b54541bf20f8",
+    "url": "/static/media/bank-note.2ac49269.svg"
+  },
+  {
+    "revision": "9a3574f3b3bed1b4dc2346919bf3427a",
+    "url": "/static/media/calendar.9a3574f3.svg"
+  },
+  {
+    "revision": "3e16854329011ee52de48173cf5d6471",
+    "url": "/static/media/cases.3e168543.svg"
+  },
+  {
+    "revision": "72f63a00f4acb617a617ded1f0052a00",
+    "url": "/static/media/deaths.72f63a00.svg"
+  },
+  {
+    "revision": "79396a448b318da2dc2f10e0989a3cb9",
+    "url": "/static/media/funder-recipient.79396a44.svg"
+  },
+  {
+    "revision": "0f2b1ae349b458d694dd6371b65844b4",
+    "url": "/static/media/funder.0f2b1ae3.svg"
+  },
+  {
+    "revision": "b312f2f090b630d182d80df2f6bff776",
+    "url": "/static/media/funding-map.b312f2f0.png"
+  },
+  {
+    "revision": "60b12033cbd09bf0e280bc203858d337",
+    "url": "/static/media/info-light.60b12033.svg"
+  },
+  {
+    "revision": "1eca5457f935b0b3c3234e547b948f3e",
+    "url": "/static/media/info.1eca5457.svg"
+  },
+  {
+    "revision": "8a7bfd97792bdc60339419df99f6b959",
+    "url": "/static/media/jee-map.8a7bfd97.png"
+  },
+  {
+    "revision": "b1f3c1733cb434a8d1cc2caed3f0d1bf",
+    "url": "/static/media/loading.b1f3c173.gif"
+  },
+  {
+    "revision": "b3c3445280095092fce28a507987131d",
+    "url": "/static/media/logo-georgetown-dark.b3c34452.png"
+  },
+  {
+    "revision": "fa1d46eda0082710d7665979c1bbd881",
+    "url": "/static/media/logo-georgetown.fa1d46ed.png"
+  },
+  {
+    "revision": "c4c63d762bc0b31496fa825e89040fa4",
+    "url": "/static/media/logo-idea-dark.c4c63d76.png"
+  },
+  {
+    "revision": "6afa1e2d582c6e8a4db3aafee44a2487",
+    "url": "/static/media/logo-talus-dark.6afa1e2d.png"
+  },
+  {
+    "revision": "10f79581952c33254ee46fde4a799128",
+    "url": "/static/media/logo-talus.10f79581.png"
+  },
+  {
+    "revision": "3a47b4ea9e3e5b084050d906158eb53d",
+    "url": "/static/media/logo-title.3a47b4ea.png"
+  },
+  {
+    "revision": "315fe3392e7d852bc99afd23e2985b3d",
+    "url": "/static/media/person.315fe339.svg"
+  },
+  {
+    "revision": "4d599c8a75601e09e6b10c1dd44affd9",
+    "url": "/static/media/plus-white.4d599c8a.svg"
+  },
+  {
+    "revision": "9078b1e5bf9a15c42f09051a7764570b",
+    "url": "/static/media/recipient.9078b1e5.svg"
+  },
+  {
+    "revision": "3fd28a7d350cbdcda08691b71b9a4aa0",
+    "url": "/static/media/sorted-asc.3fd28a7d.svg"
+  },
+  {
+    "revision": "dfe2bd32b366bc708fa11e5d08c6f245",
+    "url": "/static/media/sorted-desc.dfe2bd32.svg"
+  },
+  {
+    "revision": "55628de5e94e59dd8bb6aea127146a75",
+    "url": "/static/media/spinner.55628de5.svg"
+  },
+  {
+    "revision": "a2631b7fde43489c60004ed829fd4778",
+    "url": "/static/media/stethoscope.a2631b7f.svg"
+  },
+  {
+    "revision": "ee0ef9936f48c3603ea9ebe61f927c06",
+    "url": "/static/media/syringe.ee0ef993.svg"
+  },
+  {
+    "revision": "7cc09d80bd0509cd4cf4ab8ba90dfd34",
+    "url": "/static/media/table-funds.7cc09d80.svg"
+  },
+  {
+    "revision": "d96775386ecc218edb482b76d9be4814",
+    "url": "/static/media/tracking-dark-mode.d9677538.png"
+  },
+  {
+    "revision": "70ecc2cc2d3ee28ebad664aac4195ab9",
+    "url": "/static/media/tracking.70ecc2cc.png"
+  },
+  {
+    "revision": "b4a022b25d10c4fdd17dd30426fc5dc6",
+    "url": "/static/media/transmission.b4a022b2.svg"
+  },
+  {
+    "revision": "869c15904a52082be3c994cc7b590416",
+    "url": "/static/media/unsorted.869c1590.svg"
+  },
+  {
+    "revision": "7e9bea192a121e4b9002b08ecccf6878",
+    "url": "/static/media/virion.7e9bea19.svg"
+  },
+  {
+    "revision": "b4a9c2f1ba97f9e1a37ed287b8332a3a",
+    "url": "/static/media/virion.b4a9c2f1.svg"
+  }
+]);
